49f44ce3f0b312cbf2b571d35731bb7b9a2c1142
Alessio Piazza <alessio.piazza@sparkfabrik.com>
2020-04-16 15:42:42 +0200

    Update dinghy.rb

--- Formula/dinghy.rb
+++ Formula/dinghy.rb
@@ -1,8 +1,8 @@
 class Dinghy < Formula
   desc "faster, friendlier Docker on OS X"
   homepage "https://github.com/codekitchen/dinghy"
-  url "https://github.com/paolomainardi/dinghy.git", :tag => "v4.4.3-fork-2", :revision => "a18d9911eef7a7021ead05f5fee004203759baa2"
-  head "https://github.com/paolomainardi/dinghy.git", :branch => :master
+  # url "https://github.com/alessiopiazza/dinghy.git", :tag => "v4.4.3-fork-2", :revision => "a18d9911eef7a7021ead05f5fee004203759baa2"
+  head "https://github.com/alessiopiazza/dinghy.git", :branch => :master
 
   depends_on "codekitchen/dinghy/unfs3"
 
